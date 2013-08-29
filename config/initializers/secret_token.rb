2c5e1beffedda2dbe5283bc32053e7c8f2fa478a
Mehmet Beydogan <mbbrain@gmail.com>
2013-08-29 14:51:25 +0300

    rails new AngularBook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularBook::Application.config.secret_key_base = 'f36db5a2f165ca4539547275893a6375df915f184c14fe1cc5451f382a35e01a1f984c501b72fc3f16d73ceb512b65840fc636ea937b07ae87a03fad95c9e7b5'
